8835d49f00f5ce06a4073f0f0fa4641a465c67fa
foxoffire33 <reinierdlp@gmail.com>
2023-09-26 01:51:54 +0200

    First token

---
+++ src/contracts/actions/write/mintTokens.ts
@@ -0,0 +1,25 @@
+import { ContractResult, PstAction, PstState } from '../../types/types';
+
+declare const ContractError;
+
+export const mintTokens = async (
+  state: PstState,
+  { caller, input: { qty } }: PstAction
+): Promise<ContractResult> => {
+  const balances = state.balances;
+
+  if(caller !== state.owner){
+    throw new ContractError('Only the owner of the contract can mint tokens.');
+  }
+
+  if (qty <= 0) {
+    throw new ContractError('Invalid token mint');
+  }
+
+  if (!Number.isInteger(qty)) {
+    throw new ContractError('Invalid value for "qty". Must be an integer');
+  }
+
+  balances[caller] ? (balances[caller] += qty) : (balances[caller] = qty);
+  return { state };
+};
